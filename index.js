01ea66522378b1a506bc2f6b33a39551e3d3639f
Henryjamesmoody <yashdumbre123@gmail.com>
2024-12-27 23:08:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express=require('express')
+const app=express();
+const path=require('path');
+const fs=require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    fs.readdir(`./files`,(err,files)=>{
+        res.render('index.ejs',{files:files}); 
+    }) 
+})
+
+app.get('/files/:fileName',(req,res)=>{
+  fs.readFile(`./files/${req.params.fileName}`,'utf8',(err,fileData)=>{
+    res.render('show',{fileName:req.params.fileName , fileData:fileData})
+    
+  })
+})
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}`,req.body.details,(err)=>{
+     res.redirect('/');
+    })
+    
+})
+
+app.listen(7171,()=>{
+    console.log('runing');
+})
